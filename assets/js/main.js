8901ce241acebc73f27cd1eb16fcbd899b11a186
Wayne Graham <wgraham@clir.org>
2021-11-01 14:19:29 -0400

    Slow down screen jitter for Chrome
    At a specifici break point (in Chrome only) slow down the interval to 1 second so the page doesn't jitter

--- assets/js/main.js
+++ assets/js/main.js
@@ -25,19 +25,14 @@
 
       const now = new Date().getTime(),
         distance = countDown - now;
-
+      
       document.getElementById("days").innerText = Math.floor(distance / (day)),
       document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
-      document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
+      document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute));
       document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
 
-      //do something later when date is reached
-      if (distance < 0) {
-        document.getElementById("headline").innerText = "It's Forum!";
-        document.getElementById("countdown").style.display = "none";
-        document.getElementById("content").style.display = "block";
-        clearInterval(x);
-      }
       //seconds
-    }, 0)
+    }, 1000)
+
+
 }());
